dfa7b2cd79bb7760d834a1c7182491c3987c7c89
hungtrn75 <hungtrn75@gmail.com>
2022-11-05 12:57:02 +0700

    git cache cleared

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = AppConfig.minSdkVersion
         targetSdk = AppConfig.targetSdkVersion
         versionCode = 1
-        versionName = "1.0"
+        versionName = "1.0.0"
         multiDexEnabled = true
         vectorDrawables {
             useSupportLibrary = true
